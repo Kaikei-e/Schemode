bd3daa263b6972cc8c9ab25d8f648ebaf28240c6
Kaikei-e <e_kaikei@protonmail.com>
2022-02-19 15:27:43 +0000

    Update: add textArea

--- schemode/components/representative.tsx
+++ schemode/components/representative.tsx
@@ -1,21 +1,49 @@
-import { Box, Flex, Input, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Text, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
 
 type Props = {
   isTheMode: boolean
 }
 
+type Opinions = {
+  readonly id: number;
+  text: string;
+};
+
 
 const RepresentativeInput = (props: Props) => {
-  if(props.isTheMode){
+  const [text, setText] = useState('');
+  const [opinions, setOpinions] = useState<Opinions[]>([]);
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newOpinion: Opinions = {
+      text: text,
+      id: new Date().getTime(),
+    };
+
+    setOpinions([newOpinion, ...opinions]);
+    setText('');
+  };
+
+  if (props.isTheMode) {
     return (
-      <Box bgColor={"blackAlpha.100"} borderRadius={"xl"} w="fit-content" h={"fit-content"} m={5}>
-        <Flex flexDir={"column"}>
-          <Input type={"text"} borderRadius={"xl"} w={"min"}></Input>
+      <Box borderRadius={"xl"} w="fit-content" h={"fit-content"} m={5} padding={"2"}>
+        <Flex flexDir={"column"} alignItems={"center"}>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}>
+            <Textarea bgColor={"gray.200"} borderRadius={"xl"} w={"min"} h={"20"} marginBottom={"2"} isRequired></Textarea>
+            <br/>
+            <Input type={"submit"} w={"fit-content"} bgColor={"gray.200"} borderRadius={"xl"} />
+          </form>
         </Flex>
       </Box>
     );
-  
-  }else{
+
+  } else {
     return <div></div>
   }
 
