12854ad7a397484a2396e78c597085273aa521b6
Kaikei-e <e_kaikei@protonmail.com>
2022-02-20 16:10:57 +0000

    UPdate: Implemented for SSG. It became Stand Alone.

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+//go:embed out/*
+//go:embed out/_next/static/*/*
+//go:embed out/_next/static/chunks/pages/*.js
+
+var SsgFS embed.FS
+
 func main(){
+	targetFS, errFS := fs.Sub(SsgFS, "out")
+	if errFS != nil {
+		log.Fatal(errFS)
+	}
+
+	http.Handle("/", http.FileServer(http.FS(targetFS)))
+
+	if errLAS := http.ListenAndServe(":9000", nil); errLAS != nil {
+		log.Fatalln(errLAS)
+	}
+
 
 }
