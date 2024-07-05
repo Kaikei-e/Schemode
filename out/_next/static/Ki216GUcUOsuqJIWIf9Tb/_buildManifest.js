b8da36566d301e0dd18be49dc344dd8d451fcdeb
Kaikei-e <e_kaikei@protonmail.com>
2024-07-05 12:55:25 +0900

    chore: Remove unused files and update build manifest files

--- out/_next/static/Ki216GUcUOsuqJIWIf9Tb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/533-8e77d04fbdb07732.js","static/chunks/pages/index-078fa5a9406adc3c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
