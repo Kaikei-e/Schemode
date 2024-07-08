cfece056063dc2c3d5aa23b33399ec5647a47418
=Kaikei-e <=e_kaikei@protonmail.com>
2024-07-09 01:48:25 +0900

    Add SSG and build manifest files

---
+++ out/_next/static/BEHGX0s86EufpGjge7Hzs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-ba441a781e409568.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
